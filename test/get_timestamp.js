9a4c89d225d14c9b10290d0509834fa737ae3343
Nguyen Thanh Binh <binhdigipro@gmail.com>
2016-11-25 10:53:12 +0700

    Improve unit test

--- test/get_timestamp.js
+++ test/get_timestamp.js
@@ -5,34 +5,35 @@ var constants = require("../constants");
 var moment = require('moment');
 
 describe("Check timestamp:", function() {
-	describe("Check timestamp length", function() {
-		var time = "";
-	    before(function(done){
-	    	timestamp.getTimestamp(constants.TIMESTAMP_URL, function(data){
-				time = data;
-				done();
-			});
-	    });
 
+	var time   = "";
+	var now ;
+	var timenow ;
+
+	before(function(done){
+    	timestamp.getTimestamp(constants.TIMESTAMP_URL, function(data){
+			time = data;
+			now = moment();
+			timenow = now.format('YYYY-MM-DD HH:mm Z');
+			done();
+		});
+    });
+
+	describe("Check timestamp length", function() {
 		it("Time stamp length is 16", function() {
 			expect(time.length).equal(16);
 		});
 	});
 
 	describe("Check convertable timestamp", function() {
-		it("Time stamp is convertable", function() {
-			var time   = "";
-			before(function(done){
-		    	timestamp.getTimestamp(constants.TIMESTAMP_URL, function(data){
-					time = data;
-					done();
-				});
-		    });
+		it("Timestamp is convertable", function() {
+			expect(moment(time/1000).isValid()).equal(true);
+		});
+	});
 
-			it("Time stamp length is 16", function() {
-				expect(moment(time/1000).isValid()).equal(true);
-			});
-			
+	describe("Check timestamp equal with timestamp of system", function() {
+		it("Time stamp is equal with system", function() {
+			expect(moment(time/1000).format('YYYY-MM-DD HH:mm Z')).equal(timenow);
 		});
 	});
 });
